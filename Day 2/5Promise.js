3a758ddfd1b8e0ba36d192e72be055f3bfd7e7d7
OMKAR AGRAWAL <10913160+Omkaragrawal@users.noreply.github.com>
2024-06-19 03:42:36 +0530

    Add Promise Examples

---
+++ Day 2/5Promise.js
@@ -0,0 +1,52 @@
+const defaultWaitPeriod = 2000;
+
+const votingBooth = (age, parameters) => new Promise((resolve, reject) => {
+    console.log('A person has entered the voting centre.');
+
+    setTimeout(() => {
+        if (age < 18) {
+            reject(new Error('The person is not allowed to vote'));
+        } else {
+            resolve(parameters);
+        }
+    }, defaultWaitPeriod);
+});
+
+votingBooth(26, 'The person has successfully voted and').then (
+    function (parameters) {
+        console.log('Indelible ink application process started.');
+        
+        setTimeout(() => {}, defaultWaitPeriod);
+
+        console.log('Indelible ink application process completed.');
+        return parameters;
+    }
+).then(
+    function (parameters) {
+        console.log('A person has started the process of punching in a vote in EVM.');
+    
+        setTimeout(() => {}, defaultWaitPeriod)
+
+        console.log('A person has completed the process of punching in a vote in EVM.');
+
+        return parameters;
+    }
+).then(
+    function (parameters) {
+        console.log('The person has started the process of verifying the voter slip in VVPAT machine.');
+    
+        setTimeout(() => parameters, defaultWaitPeriod)
+
+        console.log('The person has completed the process of verifying the voter slip in VVPAT machine.');
+
+        return parameters;
+    }
+).then(
+    function (parameters) {
+        console.log(parameters + ' has successfully completed the final step of submitting the vote.');
+    }
+).then (
+    () => console.log('A person has exited the voting centre.')
+).then (
+    () => console.log('After the I/O operation is completed.')
+);
---
+++ Day 2/6PromiseChain.js
@@ -0,0 +1,47 @@
+const defaultWaitPeriod = 2000;
+
+const votingBooth = (age, parameters) => new Promise((resolve, reject) => {
+    console.log('A person has entered the voting centre.');
+
+    setTimeout(() => {
+        if (age < 18) {
+            reject(new Error('The person is not allowed to vote'));
+        } else {
+            resolve(parameters);
+        }
+    }, defaultWaitPeriod);
+});
+
+const defaultPromiseFunction = function (parameters) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(parameters), defaultWaitPeriod);
+    });
+}
+
+votingBooth(26, 'The person has successfully voted and').then (
+    (parameters) => {
+        console.log('Indelible ink application process started.');
+        return defaultPromiseFunction(parameters);
+    }
+).then(
+    (parameters) => {
+        console.log('Indelible ink application process completed.');
+        console.log('A person has started the process of punching in a vote in EVM.');
+
+        return defaultPromiseFunction(parameters);
+    } 
+).then(
+    (parameters) => {
+        console.log('A person has completed the process of punching in a vote in EVM.');
+        console.log('The person has started the process of verifying the voter slip in VVPAT machine.');
+
+        return defaultPromiseFunction(parameters);;
+    }
+).then(
+    function (parameters) {
+        console.log('The person has completed the process of verifying the voter slip in VVPAT machine.');
+        console.log(parameters + ' has successfully completed the final step of submitting the vote.');
+        console.log('A person has exited the voting centre.');
+        console.log('After the I/O operation is completed.');
+    }
+);
